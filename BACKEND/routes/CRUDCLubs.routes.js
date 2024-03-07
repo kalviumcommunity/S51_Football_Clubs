3801de15640d20a2b6a0609d966e96fcd378b26d
HarishKarthickS <141898754+HarishKarthickS@users.noreply.github.com>
2024-03-07 17:53:26 +0530

    Update CRUDCLubs.routes.js

--- BACKEND/routes/CRUDCLubs.routes.js
+++ BACKEND/routes/CRUDCLubs.routes.js
@@ -28,7 +28,7 @@ const schema = Joi.object({
     const authenticateToken = (req, res,next) => {
         const authHeader = req.headers['authorization'];
         const token = authHeader && authHeader.split(' ')[1]
-        if(token) return res.sendStatus(401)
+        if(token) return res.status(401).send(token)
         jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
           if(err) return res.status(403).send(err);
           req.user = user
